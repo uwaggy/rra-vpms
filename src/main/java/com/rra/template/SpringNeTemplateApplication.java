0978b70ccbe61093db1e4fdfc5ba3aab0c828226
Lydia Numwali <numwalilydia2022@gmail.com>
2025-05-12 21:45:44 +0200

    initial

---
+++ src/main/java/com/rra/template/SpringNeTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.rra.template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "com.vanessa")
+public class SpringNeTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringNeTemplateApplication.class, args);
+    }
+
+}
